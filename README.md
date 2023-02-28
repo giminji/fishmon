0a6836bb94a70dbc763a1d408ce86fbf2dc8fecb
Evan Edwards <evan@ejedev.com>
2023-02-27 18:58:56 -0800

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # fishmon
 Ashita addon for fishing in FFXI
+
+## Features
+- Replaces the default fishing result message (ex: `Something caught the hook!`) with a colour coded message showing whether it is a Small Fish, Big Fish, Item, or Monster.
+- Adds a GUI that tracks total fish caught, total profit, and profit per hour (from first fish catch.)
+- Settings to configure custom prices on a per fish basis. Can be useful if you swap between NPCing or AHing a fish or if prices change often.
+- Outputs a debug message relating to fish stats (Fish HP, how long you have to catch it, how many arrows you need to hit, etc.)
+
+## Setup
+
+In your Ashita addons folder, create a new folder called `fishmon` and put the latest `fishmon.lua` file into it.
+
+The first time you load the addon, a new folder called `settings` will be created in the `fishmon` folder. It will have a file called `settings.json` in it that looks something like this:
+
+```
+{
+    "prices": {
+        "5454": 600,
+        "5455": 700
+    },
+    "show_debug": true,
+    "show_gui": true
+}
+```
+The addon only comes with data for `Ahtapot` and `Mercanbaligi`. You will need to add more fish in.
+
+To do this, go to the Eden website and look at the AH page for a certain fish. The URL will update to show the `ID` of the fish. Visiting the AH page for `Black Soles`, we can see it has an ID of `4384`: `https://edenxi.com/tools/item/4384`
+
+Black Soles are currently selling for `10,500g` per stack, so we will use `875` as the value. We would then update the settings file like this:
+
+```
+{
+    "prices": {
+        "5454": 600,
+        "5455": 700,
+        "4384": 875
+    },
+    "show_debug": true,
+    "show_gui": true
+}
+```
+
+If a fish does not have a price set in the settings it will not count towards the total profit, profit per hour, or total fish caught.
+
+Note: the items in the list don't need to be fish. Any non-fish item acquired from fishing will work (like mythril swords and silver rings.)
+
+## Other Settings
+Right now the only other settings are `show_debug` and `show_gui`. The debug setting allows you to toggle whether you see info about arrows, health, and damage. The GUI setting allows you to either enable or disable the GUI. Both are enabled by default.
---
+++ fishmon.lua
@@ -0,0 +1,127 @@
+_addon.author = "Floaty"
+_addon.name = "fishmon"
+_addon.version = "1.1.0"
+
+require("common")
+----------------------------------------------------------------------------------------------------
+-- Default config
+-- A file will be generated at settings/settings.json in the addon folder
+-- Edit the settings file or add new fish in here with the format ['fishid'] = gilvalue
+----------------------------------------------------------------------------------------------------
+local default_config = {
+    prices = {},
+    show_gui = true,
+    show_debug = true,
+}
+default_config.prices = {
+    ["5455"] = 700, --Ahtapot
+    ["5454"] = 600, --Mercanbaligi
+}
+
+local player = GetPlayerEntity()
+local fishmon_config = default_config
+local fish_caught = 0
+local started = false
+local start_time = os.time(os.date("!*t")) -- Start time is temporarily set on load
+local profit = 0
+local profit_per_hour = 0
+local fishing = {}
+fishing.messages = {
+    ["Something caught the hook!!!1"] = string.format("\31\130=========================>>> \31\6Big Fish"),
+    ["Something caught the hook!1"] = string.format("\31\130=========================>>> \31\36Small Fish"),
+    ["You feel something pulling at your line."] = string.format("\31\130=========================>>> \31\05Item"),
+    ["Something clamps onto your line ferociously!"] = string.format(
+        "\31\130=========================>>> \31\39Monster"
+    ),
+}
+----------------------------------------------------------------------------------------------------
+-- Load config
+----------------------------------------------------------------------------------------------------
+ashita.register_event("load", function()
+    if not ashita.file.file_exists(_addon.path .. "/settings/settings.json") then
+        ashita.settings.save(_addon.path .. "/settings/settings.json", fishmon_config)
+    end
+
+    fishmon_config = ashita.settings.load_merged(_addon.path .. "/settings/settings.json", fishmon_config)
+end)
+----------------------------------------------------------------------------------------------------
+-- Parse packets for fish and result data
+----------------------------------------------------------------------------------------------------
+ashita.register_event("incoming_packet", function(id, size, data)
+    profit_per_hour = profit / (((os.time(os.date("!*t")) - start_time) / 60) / 60)
+    if id == 277 then -- Packet 277 has info on stamina, damage, and time
+        if fishmon_config.show_debug == true then
+            stamina = struct.unpack("H", data, 0x04 + 1)
+            dmg = struct.unpack("H", data, 0x0C + 1)
+            time = struct.unpack("H", data, 0x10 + 1)
+            print(
+                "\31\16"
+                    .. tostring(math.floor((stamina / dmg) + 0.5))
+                    .. " Arrows | "
+                    .. tostring(time)
+                    .. " Seconds | "
+                    .. tostring(stamina)
+                    .. " Health | "
+                    .. tostring(dmg)
+                    .. " Damage"
+            )
+        end
+    end
+    if id == 39 then -- Packet 39 contains the actual fish ID. It is only sent after the fish is caught
+        target = struct.unpack("H", data, 5)
+        if player.ServerId == target then
+            if (started == false) and (fish_caught == 0) then
+                start_time = os.time(os.date("!*t"))
+                started = true
+            end
+            id = tostring(struct.unpack("H", data, 17))
+            for key, v in pairs(fishmon_config.prices) do
+                if string.contains(id, key) then
+                    fish_caught = fish_caught + 1
+                    profit = profit + v
+                end
+            end
+        end
+    end
+    return false
+end)
+----------------------------------------------------------------------------------------------------
+-- Replace default message
+----------------------------------------------------------------------------------------------------
+ashita.register_event("incoming_text", function(mode, chat)
+    for key, v in pairs(fishing.messages) do
+        if string.contains(chat, key) then
+            return v
+        end
+    end
+    return false
+end)
+----------------------------------------------------------------------------------------------------
+-- Render GUI
+----------------------------------------------------------------------------------------------------
+ashita.register_event("render", function()
+    if fish_caught == 0 then -- Don't show the GUI if the user isn't fishing yet.
+        return
+    end
+    if fishmon_config.show_gui == false then
+        return
+    end
+    imgui.SetNextWindowSize(200, 100, ImGuiSetCond_Always)
+    if imgui.Begin("fishmon") == false then
+        imgui.End()
+        return
+    end
+    imgui.Text("Fish Caught:")
+    imgui.SameLine()
+    imgui.Text(fish_caught)
+    imgui.Separator()
+    imgui.Text("Total gil:")
+    imgui.SameLine()
+    imgui.Text(string.gsub(profit, "^(-?%d+)(%d%d%d)", "%1,%2"))
+    imgui.Separator()
+    imgui.Text("Gil/hr:")
+    imgui.SameLine()
+    imgui.Text(string.gsub(math.floor(profit_per_hour), "^(-?%d+)(%d%d%d)", "%1,%2"))
+    imgui.Separator()
+    imgui.End()
+end)
